bde9f5d46756508ba368804e6b54939385dff1c2
Hady <itshadyy@gmail.com>
2025-06-04 10:45:14 +0300

    Refactor Portfolio.js to include comprehensive project details and alignments, while updating Style.css to replace grid layout with a flexbox approach for improved responsiveness and clarity.

--- src/pages/Portfolio.js
+++ src/pages/Portfolio.js
@@ -14,18 +14,76 @@ function Portfolio() {
                 name: 'PRE Developments',
                 category: 'Community Application',
                 description: 'The Community Mobile Application is your one-stop solution for seamless living in your residential compound. This app connects residents to essential services, events, and each other. Enjoy features like facility booking, maintenance requests, community announcements, and exclusive deals from nearby businesses—all at your fingertips. Stay informed, engaged, and effortlessly manage your daily needs within the community.',
-                image: 'media/Portfolio/Mobile/PRE.png'
+                image: 'media/Portfolio/Mobile/PRE.png',
+                align: 'flex right'
             },
-            { id: 2, name: 'JDAR Developments', image: 'media/Portfolio/Mobile/JDAR.png' },
-            { id: 3, name: 'al ahly sabour Developments', image: 'media/Portfolio/Mobile/ahly.png' },
-            { id: 4, name: 'Buzz Mobility', image: 'media/Portfolio/Mobile/Buzz.png' },
-            { id: 5, name: 'Orange', image: 'media/Portfolio/Mobile/orange.png' },
-            { id: 6, name: 'Vinde', image: 'media/Portfolio/Mobile/vinde.png' },
+            {
+                id: 2,
+                name: 'JDAR Developments',
+                category: 'Community Application',
+                description: 'The Community Mobile Application is your one-stop solution for seamless living in your residential compound. This app connects residents to essential services, events, and each other. Enjoy features like facility booking, maintenance requests, community announcements, and exclusive deals from nearby businesses—all at your fingertips. Stay informed, engaged, and effortlessly manage your daily needs within the community.',
+                align: 'flex left',
+                image: 'media/Portfolio/Mobile/JDAR.png'
+            },
+            {
+                id: 3,
+                name: 'Al Ahly Sabour Developments',
+                category: 'Community Application',
+                description: 'The Community Mobile Application is your one-stop solution for seamless living in your residential compound. This app connects residents to essential services, events, and each other. Enjoy features like facility booking, maintenance requests, community announcements, and exclusive deals from nearby businesses—all at your fingertips. Stay informed, engaged, and effortlessly manage your daily needs within the community.',
+                align: 'flex right',
+                image: 'media/Portfolio/Mobile/ahly.png'
+            },
+            {
+                id: 4,
+                name: 'Buzz Mobility',
+                category: 'Entertainment ',
+                description: "Discover the ultimate convenience in urban transportation with Buzz Mobility! Rent electric scooters effortlessly and zip around the compound in style. Whether you're exploring new places, or simply enjoying the open road, Buzz Mobility makes scooter rental a breeze.",
+                align: 'flex left',
+                image: 'media/Portfolio/Mobile/Buzz.png'
+            },
+            {
+                id: 6,
+                name: 'Vinde',
+                category: 'Entertainment ',
+                description: "Vinde is an exciting application that offers a unique way to enjoy video games. With its variety of games and amazing prizes, it is sure to provide hours of entertainment. Download Vinde now and start your journey to becoming a champion!",
+                align: 'flex',
+                image: 'media/Portfolio/Mobile/vinde.png'
+            },
+            {
+                id: 5,
+                name: 'Orange Pharmacies ',
+                category: 'Shopping, Pharmaceuticals ',
+                description: "Orange Pharmacies is a user-friendly mobile application designed to make purchasing pharmacy products seamless and convenient. Whether you need prescription medications, wellness essentials, or healthcare supplies, Orange Pharmacies ensures a hassle-free shopping experience.",
+                align: 'flex left',
+                image: 'media/Portfolio/Mobile/orange.png'
+            },
+
         ],
         websites: [
-            { id: 7, name: 'Arab Dairy Website', image: 'media/Portfolio/Websites/Arab Dairy.png' },
-            { id: 8, name: 'Orange Website', image: 'media/Portfolio/Websites/Orange.png' },
-            { id: 9, name: 'Pioneers Webite', image: 'media/Portfolio/Websites/Pioneers.png' },
+            {
+                id: 6,
+                name: 'Arab Dairy Website',
+                category: 'Shopping, Pharmaceuticals ',
+                description: "The Community Mobile Application is your one-stop solution for seamless living in your residential compound. This app connects residents to essential services, events, and each other. Enjoy features like facility booking, maintenance requests, community announcements, and exclusive deals from nearby businesses—all at your fingertips. Stay informed, engaged, and effortlessly manage your daily needs within the community.",
+                align: 'flex',
+                image: 'media/Portfolio/Websites/Arab Dairy.png'
+            },
+            {
+                id: 7,
+                name: 'Orange Website',
+                category: 'Shopping, Pharmaceuticals ',
+                description: "The Community Mobile Application is your one-stop solution for seamless living in your residential compound. This app connects residents to essential services, events, and each other. Enjoy features like facility booking, maintenance requests, community announcements, and exclusive deals from nearby businesses—all at your fingertips. Stay informed, engaged, and effortlessly manage your daily needs within the community.",
+                align: 'flex left',
+                image: 'media/Portfolio/Websites/Orange.png'
+            },
+            {
+                id: 8,
+                name: 'Pioneers Webite',
+                category: 'Shopping, Pharmaceuticals ',
+                description: "The Community Mobile Application is your one-stop solution for seamless living in your residential compound. This app connects residents to essential services, events, and each other. Enjoy features like facility booking, maintenance requests, community announcements, and exclusive deals from nearby businesses—all at your fingertips. Stay informed, engaged, and effortlessly manage your daily needs within the community.",
+                align: 'flex',
+                image: 'media/Portfolio/Websites/Pioneers.png'
+            },
         ],
     };
 
@@ -39,12 +97,25 @@ function Portfolio() {
                 return (
                     <>
                         <div className="projects-section">
-                            <h2>Mobile Application Projects</h2>
                             <div className="projects-list">
                                 {projects.mobile.map(project => (
-                                    <div key={project.id} className="project-item" onClick={() => handleProjectClick(project.id)}>
-                                        <img src={project.image} alt={project.name} />
-                                        <p>{project.name}</p>
+                                    <div key={project.id} className={project.align}>
+                                        <div>
+                                            <h2>{project.name}</h2>
+                                            <span>{project.category}</span>
+                                            <p>{project.description}</p>
+
+                                            <div className="btn-container">
+                                                <span className="btn">Android</span>
+                                                <span className="btn">iOS</span>
+                                                <span className="btn">Mobile</span>
+                                            </div>
+                                            <Link to="/">View Projects <img src="media/assets/arrow02.png" alt="arrow" /></Link>
+                                        </div>
+
+                                        <div className="flex-img">
+                                            <img src={project.image} alt="Projects" />
+                                        </div>
                                     </div>
                                 ))}
                             </div>
@@ -55,12 +126,25 @@ function Portfolio() {
                 return (
                     <>
                         <div className="projects-section">
-                            <h2>Website Projects</h2>
                             <div className="projects-list">
                                 {projects.websites.map(project => (
-                                    <div key={project.id} className="project-item" onClick={() => handleProjectClick(project.id)}>
-                                        <img src={project.image} alt={project.name} />
-                                        <p>{project.name}</p>
+                                    <div key={project.id} className={project.align}>
+                                        <div>
+                                            <h2>{project.name}</h2>
+                                            <span>{project.category}</span>
+                                            <p>{project.description}</p>
+
+                                            <div className="btn-container">
+                                                <span className="btn">Windows</span>
+                                                <span className="btn">MacOS</span>
+                                                <span className="btn">PC</span>
+                                            </div>
+                                            <Link to="/">View Projects <img src="media/assets/arrow02.png" alt="arrow" /></Link>
+                                        </div>
+
+                                        <div className="flex-img">
+                                            <img src={project.image} alt="Projects" />
+                                        </div>
                                     </div>
                                 ))}
                             </div>
