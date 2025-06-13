582c083e37f076ea7246bd5ac6a0af4cd5f49774
Hady <itshadyy@gmail.com>
2025-06-13 13:07:36 +0300

    Implement automatic horizontal scrolling for client logos in Clients.js. Update Style.css to add styles for the scrolling container and hide overflow, enhancing visual presentation and user engagement.

--- src/components/Clients.js
+++ src/components/Clients.js
@@ -1,15 +1,52 @@
-function Clients(){
-    return(
+import React, { useEffect, useRef } from 'react';
+
+function Clients() {
+    const scrollRef = useRef(null);
+
+    useEffect(() => {
+        const scrollContainer = scrollRef.current;
+        if (!scrollContainer) return;
+
+        let scrollAmount = 0;
+        const scrollSpeed = 0.5; // Adjust scroll speed as needed
+        const intervalTime = 10; // milliseconds for update
+
+        const scroll = () => {
+            if (scrollContainer.scrollWidth > scrollContainer.clientWidth) {
+                scrollAmount += scrollSpeed;
+                if (scrollAmount >= scrollContainer.scrollWidth - scrollContainer.clientWidth) {
+                    scrollAmount = 0; // Reset scroll to beginning
+                }
+                scrollContainer.scrollLeft = scrollAmount;
+            } else {
+                scrollAmount = 0; // No need to scroll if content fits
+            }
+        };
+
+        const interval = setInterval(scroll, intervalTime);
+
+        return () => clearInterval(interval);
+    }, []);
+
+    return (
         <div className="clients">
-
-            <img src="media/clients/logo01.png" />
-            <img src="media/clients/logo02.png" />
-            <img src="media/clients/logo03.png" />
-            <img src="media/clients/logo04.png" />
-            <img src="media/clients/logo05.svg" />
-            <img src="media/clients/logo06.png" />
-            <img src="media/clients/logo07.png" />
-
+            <div className="clients-scroll-container" ref={scrollRef}>
+                <img src="media/clients/logo01.png" alt="Client Logo 1" />
+                <img src="media/clients/logo02.png" alt="Client Logo 2" />
+                <img src="media/clients/logo03.png" alt="Client Logo 3" />
+                <img src="media/clients/logo04.png" alt="Client Logo 4" />
+                <img src="media/clients/logo05.svg" alt="Client Logo 5" />
+                <img src="media/clients/logo06.png" alt="Client Logo 6" />
+                <img src="media/clients/logo07.png" alt="Client Logo 7" />
+                {/* Duplicate images for seamless looping */}
+                <img src="media/clients/logo01.png" alt="Client Logo 1" />
+                <img src="media/clients/logo02.png" alt="Client Logo 2" />
+                <img src="media/clients/logo03.png" alt="Client Logo 3" />
+                <img src="media/clients/logo04.png" alt="Client Logo 4" />
+                <img src="media/clients/logo05.svg" alt="Client Logo 5" />
+                <img src="media/clients/logo06.png" alt="Client Logo 6" />
+                <img src="media/clients/logo07.png" alt="Client Logo 7" />
+            </div>
         </div>
     );
 }
